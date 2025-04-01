b9b11810242f0a996f9bf3bb544b4393bef1d3f5
Yaguex <hectoryaguemail@gmail.com>
2025-04-01 22:48:28 +0700

    ReturnvsShortPut 1

--- src/components/diy-dividend/DividendMetrics.tsx
+++ src/components/diy-dividend/DividendMetrics.tsx
@@ -36,9 +36,6 @@ export function DividendMetrics({ dividend }: DividendMetricsProps) {
   const putFee = putContracts * dividend.strike_put_mid * 100
   const totalFee = callFee + putFee
 
-  // What would be the Put premium
-  const putFeeAtCallStrike = putContracts * dividend.strike_put_mid * 100  
-
   // Calculate money earned through standard dividends
   const totalDividend = underlyingShares * dividend.underlying_price * (dividend.dividend_yield / 100) * yearsUntilExpiration
 
@@ -58,7 +55,8 @@ export function DividendMetrics({ dividend }: DividendMetricsProps) {
   const ReturnvsBond = maxAnnualROI / dividend.bond_yield
 
   // Calculate maxAnnualROI vs Risk free rate ratio
-  const ReturnvsShortPut = totalIncome / (putFeeAtCallStrike + (dividend.nominal * (dividend.bond_yield / 100) * yearsUntilExpiration))
+  const putFeeIfNotDIYDividend = Math.round(((dividend.nominal / dividend.strike_put)/100) * dividend.strike_put_mid * 100) // Calculate how many short puts we'd have if we didnt go for a DIY Dividend structure.
+  const ReturnvsShortPut = totalIncome / (putFeeIfNotDIYDividend + (dividend.nominal * (dividend.bond_yield / 100) * yearsUntilExpiration))
 
 
   // Determine the text color based on value above or below 0
