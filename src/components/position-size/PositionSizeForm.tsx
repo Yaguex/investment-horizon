60506b884aebb2bd6b57026913b21adc573bf756
Yaguex <hectoryaguemail@gmail.com>
2025-04-04 16:07:30 +0700

    text

--- src/components/position-size/PositionSizeForm.tsx
+++ src/components/position-size/PositionSizeForm.tsx
@@ -105,7 +105,7 @@ export function PositionSizeForm({ open, onOpenChange, position }: PositionSizeF
     }
   }
 
-  // The Form fields go below
+  // The Form fields go below...
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
       <SheetContent className="w-[400px] sm:w-[540px]">
