4f46bdbb368c7b4a9f8c0be0d455106d5f8bcf51
Yaguex <hectoryaguemail@gmail.com>
2025-04-05 16:50:50 +0700

    css 1

--- src/components/position-size/PositionMetrics.tsx
+++ src/components/position-size/PositionMetrics.tsx
@@ -83,7 +83,7 @@ export function PositionMetrics({ position }: positionMetricsProps) {
         
 
         {/* Below goes the small numbers in the bottom left of the display*/}
-        <div class="mt-2 relative">
+        <div className="mt-2 relative">
         <p>
             <Tooltip>
               <TooltipTrigger>
