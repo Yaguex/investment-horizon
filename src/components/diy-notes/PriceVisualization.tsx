4ec7cecfd04e825f5c106232d441981c171a3a78
Yaguex <hectoryaguemail@gmail.com>
2025-04-08 15:28:14 +0700

    leverage 11

--- src/components/diy-notes/PriceVisualization.tsx
+++ src/components/diy-notes/PriceVisualization.tsx
@@ -31,8 +31,8 @@ const calculateCirclePositions = (note: any) => {
     ((note.strike_target_mid * 100) - (note.strike_entry_mid * 100))
   )
 
-  // Calculate leverage
-  const leverage = entryContracts / ((1000000 + (note.nominal * (note.dividend_yield/100) * yearsUntilExpiration) - (totalBondYield + (protectionContracts * note.strike_protection_mid * 100) + (entryContracts * note.strike_entry_mid * 100 * -1) + (entryContracts * note.strike_target_mid * 100))) / note.strike_entry / 100)
+  / Calculate leverage (leverage vs underlying)
+  const leverage = maxGainDollars / (((note.nominal / note.underlying_price) * (note.strike_target - note.strike_entry)) + totalDividend)
 
   // Calculate BE strikes with updated formulas
   const be1Strike = note.strike_entry + ((note.strike_entry * ((note.bond_yield/100) * yearsUntilExpiration)) / leverage)
